163dc0f770094d2ea31db8e16093852d4a5fcae8
a1013457richar <r11722013@g.ntu.edu.tw>
2024-02-11 21:37:39 +0800

    change congif

--- next.config.js
+++ next.config.js
@@ -8,7 +8,19 @@ const nextConfig = {
         hostname: "files.stripe.com"
       }
     ]
-  }
+  },
+  async exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return Object.keys(defaultPathMap).reduce((pathMap, path) => {
+      if (path.includes('/api/')) {
+        return pathMap;
+      } else {
+        return {
+          ...pathMap,
+          [path]: defaultPathMap[path],
+        };
+      }
+    }, {});
+  },
 }
 
 module.exports = nextConfig
